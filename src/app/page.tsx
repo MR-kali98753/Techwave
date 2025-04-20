e4b6b2a5c16a4e71cb8d1db79fedf5f571eab8ee
MR kali <mrkali98753@gmail.com>
2025-04-20 18:23:26 +0530

    added blogs to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Ads from "@/components/Ads";
+import Blogs from "@/components/Blogs";
 import Navbar from "@/components/Navbar";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Navbar />
       <Ads />
+      <Blogs />
     </>
   );
 }
